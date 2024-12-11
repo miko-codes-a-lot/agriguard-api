5a50eeea4aef8cf71be9e57c2011236b4b798ff7
Miko Chu <miko.codecrafters@gmail.com>
2024-12-12 00:47:47 +0800

    improve onion insurance stream

--- src/service/notify.service.js
+++ src/service/notify.service.js
@@ -2,15 +2,20 @@ const { Notify } = require('../model')
 
 class NotifyService {
   create ({ senderId, receiverId, documentType, documentId, message }) {
-    const notify = new Notify({
-      sender: senderId,
-      receiver: receiverId,
-      documentType,
-      documentId,
-      message
-    })
+    try {
+      const notify = new Notify({
+        sender: senderId,
+        receiver: receiverId,
+        documentType,
+        documentId,
+        message
+      })
 
-    return notify.save()
+      return notify.save()
+    } catch (e) {
+      console.log('Notify.create error')
+      console.error(e.stack)
+    }
   }
 }
 
--- src/stream/onion-insurance.stream.js
+++ src/stream/onion-insurance.stream.js
@@ -2,6 +2,17 @@ const { OnionInsurance, User } = require('../model')
 
 const { notifyService } = require('../service')
 
+const notifyOnCreate = async (doc) => {
+  const user = await User.findOne({ _id: doc.createdById })
+  return notifyService.create({
+    senderId: doc.createdById,
+    receiverId: user.createdById,
+    documentType: 'onion_insurance',
+    documentId: doc._id,
+    message: `Onion insurance have been filed by ${user.firstName} ${user.lastName}`
+  })
+}
+
 module.exports = {
   OnionInsuranceStream: {
     watch: () => {
@@ -11,14 +22,7 @@ module.exports = {
         const doc = change.fullDocument
 
         if (change.operationType === 'insert') {
-          const user = await User.findOne({ _id: doc.createdById })
-          await notifyService.create({
-            senderId: doc.createdById,
-            receiverId: user.createdById,
-            documentType: 'onion_insurance',
-            documentId: doc._id,
-            message: `Onion insurance have been filed by ${user.firstName} ${user.lastName}`
-          })
+          await notifyOnCreate(doc)
         } else if (change.operationType === 'update') {
           console.log('nothing at the moment')
         }
