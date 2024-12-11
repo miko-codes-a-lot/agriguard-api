4c46ffb66e4cea32dd58cbb5ee220eba82cc8deb
Miko Chu <miko.codecrafters@gmail.com>
2024-12-11 20:18:29 +0800

    add user model

---
+++ src/model/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  firstName: String,
+  middleName: String,
+  lastName: String,
+  mobileNumber: String,
+  address: String,
+  email: String,
+  dateOfBirth: Date,
+  password: String,
+  nameOfSpouse: String,
+  isAdmin: Boolean,
+  isTechnician: Boolean,
+  isFarmers: Boolean,
+  userProfile: String,
+  validId: String,
+  createdById: mongoose.Types.ObjectId,
+  createdAt: Date,
+  lastUpdatedById: mongoose.Types.ObjectId,
+  lastUpdatedAt: Date,
+  deletedById: mongoose.Types.ObjectId,
+  deletedAt: Date
+}, {
+  versionKey: false,
+  timestamps: false,
+  toObject: { getters: true },
+  toJSON: { getters: true }
+})
+
+const User = mongoose.model('User', userSchema, 'User')
+
+module.exports = {
+  User
+}
