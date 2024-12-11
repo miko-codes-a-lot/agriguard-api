e68ac5fac291f80da994a34dcc04bec65a23fcb1
Miko Chu <miko.codecrafters@gmail.com>
2024-12-11 20:45:59 +0800

    expose models

--- src/model/index.js
+++ src/model/index.js
@@ -1,5 +1,15 @@
+const { ComplaintInsurance } = require('./complaint-insurance.model')
+const { OnionInsurance } = require('./onion-insurance.model')
+const { RiceInsurance } = require('./rice-insurance.model')
+const { Indemnity } = require('./indemnity.model')
 const { Notify } = require('./notify.model')
+const { User } = require('./user.model')
 
 module.exports = {
-  Notify
+  ComplaintInsurance,
+  OnionInsurance,
+  RiceInsurance,
+  Indemnity,
+  Notify,
+  User
 }
